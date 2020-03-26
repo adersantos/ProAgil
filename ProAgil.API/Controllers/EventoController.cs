5403d2ba62fb26c336e077375992453992cd08cd
ader santos <ader.santos@animaeducacao.com.br>
2020-03-26 13:47:47 -0300

    Atualizacao do projeto

---
+++ ProAgil.API/Controllers/EventoController.cs
@@ -0,0 +1,34 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProAgil.Repository;
+
+namespace ProAgil.API.Controllers
+{
+    public class EventoController : ControllerBase
+    {
+        private readonly IProAgilRepository _repo;
+
+        public EventoController(IProAgilRepository repo)
+        {
+            _repo = repo;
+           // _repo.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var results = await _repo.GetAllEventoAsync(true);
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError,"Banco de dados falhou.");
+            }
+        }
+    }
+}
